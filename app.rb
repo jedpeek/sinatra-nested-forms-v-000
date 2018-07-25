b7894592008695306870b082dfebe3d6c578d9fc
Jed Peek <jedpeek@yahoo.com>
2018-07-25 04:37:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@ module FormsLab
     end
 
     post '/pirates' do
-      binding.pry
       pirate = Pirate.create(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
       params[:pirate][:ships].each do |ship_data|
         ship = Ship.new(ship_data)
